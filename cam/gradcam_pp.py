c4422ad548656d21a7a929a73b9380cf249b6654
Hiroya Kawai <65449659+serlsier02@users.noreply.github.com>
2022-12-08 18:55:06 +0900

    Create gradcam_pp.py

---
+++ cam/gradcam_pp.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn.functional as F
+from gradcam import GradCAM
+
+
+class GradCAMpp(GradCAM):
+    def __init__(self, model, target_layer_path, device):
+        super().__init__(model, target_layer_path, device)
+
+    def forward(self, input, class_idx=None):
+        b, c, h, w = input.size()
+        input = input.to(self.device)
+
+        logit, _ = self.model(input)
+        if class_idx is None:
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            score = logit[:, class_idx].squeeze()
+
+        self.model.zero_grad()
+        score.backward(retain_graph=True)
+        gradients = self.gradients
+        activations = self.activations
+        b, k, u, v = gradients.size()
+
+        alpha_num = gradients.pow(2)
+        alpha_denom = gradients.pow(2).mul(2) + \
+                activations.mul(gradients.pow(3)).view(b, k, u*v).sum(-1, keepdim=True).view(b, k, 1, 1)
+        alpha_denom = torch.where(alpha_denom != 0.0, alpha_denom, torch.ones_like(alpha_denom))
+
+        alpha = alpha_num.div(alpha_denom+1e-7)
+        positive_gradients = F.relu(score.exp()*gradients) # ReLU(dY/dA) == ReLU(exp(S)*dS/dA))
+        weights = (alpha*positive_gradients).view(b, k, u*v).sum(-1).view(b, k, 1, 1)
+
+        saliency_map = (weights*activations).sum(1, keepdim=True)
+        saliency_map = F.relu(saliency_map)
+        saliency_map = F.interpolate(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+        saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
+        saliency_map = (saliency_map-saliency_map_min).div(saliency_map_max-saliency_map_min).data
+
+        return saliency_map.cpu()[0, 0]
